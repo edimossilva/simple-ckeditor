0480879ba20e96028ef02e4486fdc3487b5229a4
Edimo Silva <edimossilva@gmail.com>
2023-11-28 10:37:00 -0300

    init repo

---
+++ build/ckeditor.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Bold } from '@ckeditor/ckeditor5-basic-styles';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+declare class Editor extends ClassicEditor {
+    static builtinPlugins: (typeof Alignment | typeof Bold | typeof Essentials | typeof Paragraph)[];
+    static defaultConfig: EditorConfig;
+}
+export default Editor;
---
+++ src/ckeditor.ts
@@ -0,0 +1,39 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Bold } from '@ckeditor/ckeditor5-basic-styles';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Alignment,
+		Bold,
+		Essentials,
+		Paragraph
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+				'bold',
+				'|',
+				'undo',
+				'redo',
+				'alignment'
+			]
+		},
+		language: 'en'
+	};
+}
+
+export default Editor;
